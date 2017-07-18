a75fb06522a4091751c3fff8bf2ca8980785cf76
bachavez <bachavez@yahoo.com>
2017-07-18 16:25:44 -0700

    commit of allergyInfo route and sypmtom route

---
+++ server/routes/allergyInfo.route.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const db = require('../models');
+
+router.get('/', function(request, response){
+  //return all allergyInfos in the database
+  var promise = db.AllergyInfo.findAll()
+
+  promise.then(function(allergyInfos){
+    response.json(allergyInfos);
+  });
+});
+
+router.get('/:id', function(request,response){
+  db.AllergyInfo.findById(request.params.id).then(function(allergyInfo){
+    if (allergyInfo === null){
+      response.sendStatus(404);
+    } else{
+      response.json(allergyInfo);
+    }
+  });
+});
+
+router.post('/', function(request, response){
+  console.log(request);
+  const allergyInfo = db.AllergyInfo.build(request.body);
+
+  allergyInfo.save().then(function(newallergyInfo){
+
+    response.send(newallergyInfo);
+  });
+});
+
+
+router.put('/:id', function(request, response){
+
+  db.AllergyInfo.findById(request.params.id).then(function(allergyInfo){
+   allergyInfo.update(request.body).then(function(allergyInfo){
+      if (allergyInfo === null){
+      response.sendStatus(404);
+    } else{
+      response.json(allergyInfo);
+    }
+   });
+  });
+
+
+});
+
+router.delete('/:id', function(request,response){
+  db.AllergyInfo.findById(request.params.id).then(function(allergyInfo){
+    allergyInfo.destroy().then(function(){
+      response.json(allergyInfo);
+    });
+  });
+
+});
+
+
+module.exports = router;
