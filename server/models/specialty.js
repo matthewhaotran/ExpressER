f91946cbe4f4c4a14405d3849e584005b548c910
Frank0095 <jr_rodriguez@ymail.com>
2017-07-18 13:51:45 -0700

    Created specialty.js will try to merge

---
+++ server/models/specialty.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function (sequelize, DataTypes) {
+
+    var Specialty = sequelize.define('Specialty', {
+        specialtyName: DataTypes.STRING,
+        specialtyDescription: DataTypes.STRING
+    });
+
+    Specialty.associate = function (models) {
+        models.Specialty.hasMany(models.Doctor, {
+            as: 'doctors',
+            foreignKey: 'doctorId'
+        });
+    };
+    return Specialty;
+};
