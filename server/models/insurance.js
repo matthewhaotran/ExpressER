058bc82afc531b72afaa67eadc74431b3b0fdfe4
Keaton Lullo <keatonl@Keatons-MacBook-Pro.local>
2017-07-18 13:50:23 -0700

    Insurance model test merge with group

---
+++ server/models/insurance.js
@@ -0,0 +1,24 @@
+'use strict'
+module.exports = function (sequelize, DataTypes) {
+    var Insurance = sequelize.define('Insurance', {
+
+        companyName: DataTypes.STRING,
+        insuranceNumber: DataTypes.INTEGER,
+
+        patientId: {
+            type: DataTypes.INTEGER,
+            model: 'Patient',
+            key: 'id'
+        }
+
+    });
+
+    Insurance.associate = function (models) {
+        models.Insurance.belongsTo(models.Patient, {
+            as: 'patient',
+            foreignKey: 'patientId'
+        });
+    };
+
+    return Insurance;
+};
