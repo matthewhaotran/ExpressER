cab22a9330f413a29d28dda226e0e057194b20d0
Frank0095 <jr_rodriguez@ymail.com>
2017-07-19 09:55:47 -0700

    TEST file and moved folder

---
+++ server/routes/visit.route.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const db = require('../models');
+
+router.get('/', function(request, response){
+  //return all visits in the database
+  var promise = db.Visit.findAll()
+
+  promise.then(function(visits){
+    response.json(visits);
+  });
+});
+
+router.get('/:id', function(request,response){
+  db.Visit.findById(request.params.id).then(function(visit){
+    if (visit === null){
+      response.sendStatus(404);
+    } else{
+      response.json(visit);
+    }
+  });
+});
+
+router.post('/', function(request, response){
+  console.log(request);
+  const visit = db.Visit.build(request.body);
+
+  visit.save().then(function(newvisit){
+
+    response.send(newvisit);
+  });
+});
+
+
+router.put('/:id', function(request, response){
+
+  db.Visit.findById(request.params.id).then(function(visit){
+   visit.update(request.body).then(function(visit){
+      if (visit === null){
+      response.sendStatus(404);
+    } else{
+      response.json(visit);
+    }
+   });
+  });
+
+
+});
+
+router.delete('/:id', function(request,response){
+  db.Visit.findById(request.params.id).then(function(visit){
+    visit.destroy().then(function(){
+      response.json(visit);
+    });
+  });
+
+});
+
+
+module.exports = router;
---
+++ server/tests/routes/patient.spec.js
@@ -0,0 +1,72 @@
+const expect = require('chai').expect;
+const request = require('supertest');
+const db = require('../../models');
+
+const app = require('../../app');
+
+describe('Patients route', () => {
+
+    it('should download all patients', (done) => {
+        request(app)
+            .get('/api/patient')
+            .expect(200, done);
+    });
+
+    it('should fetch a single patient', (done) => {
+        request(app)
+            .get('/api/patient/1')
+            .expect(200, done);
+    });
+
+    it('should return 404 when fetching a nonexistent patient', (done) => {
+        request(app)
+            .get('/api/patient/9999999999999999')
+            .expect(404, done);
+    });
+
+    it('should add a patient', (done) => {
+        request(app)
+            .post('/api/patient')
+            .send({firstName: 'John', lastName: 'Doe'})
+            .expect(201, done);
+    });
+
+    it('should update a patient', (done) => {
+        const newPatient = new db.Patient({
+            firstName: 'Cisco',
+            lastName: 'Rodriguez'
+        });
+
+        newPatient
+            .save()
+            .then(patient => {
+                request(app)
+                .put('/api/patient/' + patient.id)
+                .send({firstName: 'Adrian'})
+                .expect(204)
+                .then(() => {
+                    db
+                        .Patient
+                        .findById(patient.id)
+                        .then (patient => {
+                            expect(patient.firstName).to.equal('Adrian');
+
+                            done();
+                        });
+                });
+            });
+    });
+
+    it('should delete a patient', (done) => {
+        const newPatient = new db.Patient({
+            firstName: 'Cisco',
+            lastName: 'Rodriguez'
+        });
+
+        newPatient.save().then(patient => {
+            request(app)
+                .delete('/api/patient/' + patient.id)
+                .expect(200, done);
+        });
+    });
+});
