f3b8160e44b06e491a29d5c1dbb04fe5aa6a4c9a
Frank0095 <jr_rodriguez@ymail.com>
2017-07-20 14:20:58 -0700

    Added Wizard and buttons

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -31,44 +31,50 @@
             controller: 'SplashController as SplashCtrl',
             templateUrl: 'app/splash/splash.template.html'
         });
+
+          $stateProvider.state('wizard', {
+            url: '/wizard',
+            controller: 'WizardController as wizardCtrl',
+            templateUrl: '/app/wizard/wizard.html'
+        });
         
-         $stateProvider.state('emergencyContact', {
+         $stateProvider.state('wizard.emergencyContact', {
             url: '/emergencyContact',
             controller: 'EmergencyContactController as EmergencyContactCtrl',
             templateUrl: 'app/emergencyContact/emergencyContact.template.html'
         });
 
-        $stateProvider.state('success', {
+        $stateProvider.state('wizard.success', {
             url: '/success',
             controller: 'SuccessController as successCtrl',
             templateUrl: 'app/success/success.template.html'
         });
 
-        $stateProvider.state('symptom', {
+        $stateProvider.state('wizard.symptom', {
             url: '/symptom',
             controller: 'SymptomController as symptomCtrl',
             templateUrl: 'app/symptom/symptom.template.html'
         });
 
-        $stateProvider.state('consent', {
+        $stateProvider.state('wizard.consent', {
             url: '/consent',
             controller: 'ConsentController as consentCtrl',
             templateUrl: 'app/consent/consent.template.html'
         });
       
-        $stateProvider.state('patient', {
+        $stateProvider.state('wizard.patient', {
             url: '/patient',
             controller: 'PatientInfoController as patientInfoCtrl',
             templateUrl: 'app/patientInfo/patientInfo.template.html'
         });
 
-        $stateProvider.state('confirmation', {
+        $stateProvider.state('wizard.confirmation', {
             url: '/confirmation',
             controller: 'ConfirmationController as confirmCtrl',
             templateUrl: 'app/confirmation/confirmation.template.html'
         });
       
-        $stateProvider.state('painScale', {
+        $stateProvider.state('wizard.painScale', {
             url: '/painScale',
             controller: 'PainScaleController as painScaleCtrl',
             templateUrl: 'app/painScale/painScale.template.html'
