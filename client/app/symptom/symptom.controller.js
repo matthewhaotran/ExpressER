f631b034ca12165eb0a16ec6fff03ba526b9ea59
Casey Ledbetter <caseyledbetter@Caseys-MacBook-Pro.local>
2017-07-21 10:26:45 -0700

    stuff for checkboxes, first to get it to win gets hot cheetos

--- client/app/symptom/symptom.controller.js
+++ client/app/symptom/symptom.controller.js
@@ -1,20 +1,38 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('app.symptom')
         .controller('SymptomController', SymptomController);
 
-    SymptomController.$inject = ['$stateParams','$state'];
-    function SymptomController($stateParams,$state) {
+    SymptomController.$inject = ['$stateParams', '$state'];
+
+    function SymptomController($stateParams, $state) {
         var vm = this;
-        
+
 
         activate();
 
         ////////////////
 
-        function activate() { }
-        
+        function activate() {
+            vm.items = [1, 2, 3, 4, 5];
+            vm.selected = [];
+
+            vm.toggle = function (item, list) {
+                var idx = list.indexOf(item);
+                console.log(idx);
+                if (idx > -1) {
+                    list.splice(idx, 1);
+                } else {
+                    list.push(item);
+                }
+            };
+
+            vm.exists = function (item, list) {
+                return list.indexOf(item) > -1;
+            };
+        }
+
     }
 })();
