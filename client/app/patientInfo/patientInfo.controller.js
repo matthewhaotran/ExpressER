05d4ca0e6ae74187247ea08338597e18141879cf
bachavez <bachavez@yahoo.com>
2017-07-24 15:43:55 -0700

    changes to PatientInfo

--- client/app/patientInfo/patientInfo.controller.js
+++ client/app/patientInfo/patientInfo.controller.js
@@ -5,9 +5,9 @@
         .module('app.patientInfo')
         .controller('PatientInfoController', PatientInfoController);
 
-    PatientInfoController.$inject = ['$stateParams', '$state', 'patientFactory'];
+    PatientInfoController.$inject = ['$stateParams', '$state', 'patientFactory', 'insuranceFactory'];
 
-    function PatientInfoController($stateParams, $state, patientFactory) {
+    function PatientInfoController($stateParams, $state, patientFactory, insuranceFactory) {
         var vm = this;
 
         vm.save = save;
@@ -18,15 +18,22 @@
 
         //function activate() {}
 
-        function save(patient){
+        function save(patient, insurance){
             patientFactory
                 .create(patient)
                 .then(function(patient){
-                    alert('patient Created!');
+                    const patientId = patient.id;
+                    const insuranceInfo = {
+                        'companyName': insurance.companyName,
+                        'insuranceNumber': insurance.insuranceNumber,
+                        'patientId': patient.id
+                    };
+
+                    return insuranceFactory.create(insuranceInfo);
+                })
+                .then(function(insurance) {
                     $state.go('emergencyContact', {id: patient.id});
-                });
-             
-            
+                });            
         }
 
     }
