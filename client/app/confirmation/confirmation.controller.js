d05631ef47120179331a1102eb6f196a165fb255
Casey Ledbetter <caseyledbetter@Caseys-MacBook-Pro.local>
2017-07-27 17:47:40 -0700

    added text message functionality to app. as well as .env

--- client/app/confirmation/confirmation.controller.js
+++ client/app/confirmation/confirmation.controller.js
@@ -5,19 +5,28 @@
 		.module('app.confirmation')
 		.controller('ConfirmationController', ConfirmationController);
 
-	ConfirmationController.$inject = ['$stateParams', '$state', 'patientFactory', 'visitFactory', 'patientSymptomFactory'];
+	ConfirmationController.$inject = ['$stateParams', '$state', 'patientFactory', 'visitFactory', 'twilioFactory'];
 
-	function ConfirmationController($stateParams, $state, patientFactory, visitFactory) {
+	function ConfirmationController($stateParams, $state, patientFactory, visitFactory, twilioFactory) {
 		/* jshint validthis:true */
 		var vm = this;
+		vm.text = text;
 
 		activate();
 
 		function activate() {
 			visitFactory
 				.getById($stateParams.state2[0])
-				.then(function(visit){
+				.then(function (visit) {
 					vm.visit = visit;
+				});
+		}
+
+		function text(number) {
+			twilioFactory
+				.sendText(number)
+				.then(function () {
+					alert('Text Sent');
 				});
 		}
 	}
